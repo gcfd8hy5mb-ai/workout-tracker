fad67e2f687ef2013e0eb98e130dcaf476528c4d
gcfd8hy5mb-ai <gcfd8hy5mb@privaterelay.appleid.com>
2026-09-26 10:41:39 -0500

    Cache Post-Workout Coach v1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "prism-v10.3-beta19";
+const CACHE_NAME = "prism-v10.3-beta20";
 
 const APP_FILES = [
   "./",
@@ -15,9 +15,10 @@ const APP_FILES = [
   "./in-workout-coach.js?v=2",
   "./set-coach.js?v=1",
   "./adaptive-set-coach.js?v=1",
-  "./next-session-coach.js?v=3",
+  "./next-session-coach.js?v=4",
   "./session-readiness.js?v=1",
   "./preworkout-plan.js?v=2",
+  "./post-workout-coach.js?v=1",
   "./images/app-icon.png",
   "./images/app-icon-512.png",
   "./images/app-icon-192.png",
