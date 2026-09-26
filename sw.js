93805f54dc83052352c8365f210eab7ad312b404
gcfd8hy5mb-ai <gcfd8hy5mb@privaterelay.appleid.com>
2026-09-26 10:05:43 -0500

    Cache Set Coach v1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "prism-v10.3-beta13";
+const CACHE_NAME = "prism-v10.3-beta14";
 
 const APP_FILES = [
   "./",
@@ -13,6 +13,7 @@ const APP_FILES = [
   "./coach-engine.js?v=3",
   "./ask-prism.js?v=1",
   "./in-workout-coach.js?v=2",
+  "./set-coach.js?v=1",
   "./images/app-icon.png",
   "./images/app-icon-512.png",
   "./images/app-icon-192.png",
