c8c1e832ab0be2705f47fd65a824ea076df642a7
gcfd8hy5mb-ai <gcfd8hy5mb@privaterelay.appleid.com>
2026-09-26 10:23:43 -0500

    Cache Next Session Coach v1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "prism-v10.3-beta16";
+const CACHE_NAME = "prism-v10.3-beta17";
 
 const APP_FILES = [
   "./",
@@ -15,6 +15,7 @@ const APP_FILES = [
   "./in-workout-coach.js?v=2",
   "./set-coach.js?v=1",
   "./adaptive-set-coach.js?v=1",
+  "./next-session-coach.js?v=1",
   "./images/app-icon.png",
   "./images/app-icon-512.png",
   "./images/app-icon-192.png",
