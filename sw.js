f80d9c0cecf511516035112f31c91594129c2f75
gcfd8hy5mb-ai <gcfd8hy5mb@privaterelay.appleid.com>
2026-09-26 10:09:27 -0500

    Cache adaptive next-set coaching

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "prism-v10.3-beta14";
+const CACHE_NAME = "prism-v10.3-beta15";
 
 const APP_FILES = [
   "./",
@@ -14,6 +14,7 @@ const APP_FILES = [
   "./ask-prism.js?v=1",
   "./in-workout-coach.js?v=2",
   "./set-coach.js?v=1",
+  "./adaptive-set-coach.js?v=1",
   "./images/app-icon.png",
   "./images/app-icon-512.png",
   "./images/app-icon-192.png",
